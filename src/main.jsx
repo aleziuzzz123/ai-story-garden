b05169fcd30c02bfb4296c9fb18649274dc95d58
aleziuzzz123 <aichatsyapp@gmail.com>
2025-07-20 01:25:31 +0000

    fix: Initialize i18n in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import './i18n';
+// src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App';
-import '@/index.css';
+import App from './App.jsx';
+import './index.css';
+
+// THIS IS THE MISSING LINE THAT ACTIVATES THE TRANSLATIONS
+import './i18n'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
